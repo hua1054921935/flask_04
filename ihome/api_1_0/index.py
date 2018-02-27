e2b19449dee30a7e03f69b162c0c4c9fc85ba3cb
xwp <xwp_fullstack@163.com>
2018-02-27 21:36:42 +0800

    项目的基本完成

--- ihome/api_1_0/index.py
+++ ihome/api_1_0/index.py
@@ -1,9 +1,14 @@
 # coding=utf-8
 # 使用蓝图对象
+import logging
 from . import api
+from ihome import redis_store
 
-@api.route('/')
-def hello_world():
+#TODO 记得导入models否则无法迁移成功
+# 操作步骤init migrate upgrade
+from ihome import models
 
-    return 'hello flask'
+@api.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return 'index'
 
